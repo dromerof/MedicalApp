beb4c88e65be1ef4abc60ba26aed667b001f6669
Daniel Romero <dandres508@gmail.com>
2024-09-04 13:49:07 -0500

    Cambios en main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Daniel Romero";
+        myDoctor.showName();
     }
 }
